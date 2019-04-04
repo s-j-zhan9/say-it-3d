b078f6fe545eae467195f8fd907963d07e1fa745
S. J. Zhang <sj@s-j-zhang.com>
2019-04-03 20:29:58 -0400

    new-example

---
+++ Emoji Bling/SuperUsefulCode/StringExtension.swift
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Adapted from https://stackoverflow.com/a/38809531
+
+import UIKit
+
+extension String {
+  
+  func image() -> UIImage? {
+    
+    let size = CGSize(width: 120, height: 22)
+    
+    UIGraphicsBeginImageContextWithOptions(size, false, 0)
+    UIColor.clear.set()
+    
+    let rect = CGRect(origin: .zero, size: size)
+    UIRectFill(CGRect(origin: .zero, size: size))
+    
+//    var myFont = UIFont(name: "HelveticaNeue", size: CGFloat(18))
+//    myFont.textColor = .white
+//
+//    (self as AnyObject).draw(in: rect, withAttributes: [.font: myFont])
+    
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.alignment = .center
+    
+    let attributes: [NSAttributedString.Key : Any] = [
+        .paragraphStyle: paragraphStyle,
+        .font: UIFont(name: "Avenir-Medium", size: CGFloat(20)),
+        .foregroundColor: UIColor.white
+    ]
+
+    
+//    let myText = ""
+//    let attributedString = NSAttributedString(string: myText, attributes: attributes)
+    
+    (self as AnyObject).draw(in: rect, withAttributes: attributes)
+    
+//    attributedString.draw(in: rect)
+    
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
+    
+    return image
+  }
+}
+
